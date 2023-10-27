802c4b71b86fee37e13d5b404f22df1e256cceaa
liam marchesi <147132249+liam-m1@users.noreply.github.com>
2023-10-27 12:53:29 -0700

    Added playGame function to decide winner of game, added youWin/youLose function for output of decision. Globalized uppercase

--- javascript.js
+++ javascript.js
@@ -1,18 +1,55 @@
+//variables
 let i = 0;
+//prompt for player choicef
+let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+let upPlayerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
 
+//generating comptuer choice
 function getComputerChoice() {
     i = Math.floor(Math.random() * 3);
-    let choice = "";
+    let compChoice;
     if (i == 0) {
-        choice = "Rock";
+        compChoice = "Rock";    
     }
     else if (i == 1) {
-        choice = "Paper";
+        compChoice = "Paper";
     }
     else {
-        choice = "Scissor"
+        compChoice = "Scissors"
     }
-    return choice;
+    return compChoice;
 }
 
-console.log(getComputerChoice());
+function youWin(upPlayerChoice, compChoice) {
+    return "You won! " + upPlayerChoice + " beats " + compChoice;
+}
+function youLost(upPlayerChoice, compChoice) {
+    return "You lost! " + compChoice + " beats " + upPlayerChoice;
+}
+
+    const compChoice = getComputerChoice();
+
+function playGame(compChoice, upPlayerChoice) {
+    if (upPlayerChoice == compChoice) {
+        "You drew! " + upPlayerChoice + " is the same as " + compChoice
+    }
+    else if (upPlayerChoice == "Rock" && compChoice == "Paper") {
+        return youLost(upPlayerChoice, compChoice)
+    }
+    else if (upPlayerChoice == "Rock" && compChoice == "Scissors") {
+        return youWin(upPlayerChoice, compChoice)
+    }
+    else if (upPlayerChoice == "Paper" && compChoice == "Scissors") {
+        return youLost(upPlayerChoice, compChoice)
+    }
+    else if (upPlayerChoice == "Paper" && compChoice == "Rock") {
+        return youWin(upPlayerChoice, compChoice)
+    }
+    else if (upPlayerChoice == "Scissors" && compChoice == "Rock")
+        return youLost(upPlayerChoice, compChoice)
+    else if (upPlayerChoice == "Scissors" && compChoice == "Paper") {
+        return youWin(upPlayerChoice, compChoice)
+    }
+}
+
+console.log(playGame(compChoice, upPlayerChoice))
